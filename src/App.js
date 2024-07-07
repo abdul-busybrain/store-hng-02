6fcdc3b5e4ba7031f2e9189652f8f6768f4962ee
Abdullahi(busybrain) <awwalhajine@gmail.com>
2024-07-07 17:42:46 +0100

    header ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// function Header() {
+//   return (
+//     <div className="Header">
+//       <img className="search" src={require("./img/home.png")} alt={"Hy"} />
+
+//       <span>
+//         Jiggyl <span>Stores</span>
+//       </span>
+
+//       <input type="search" placeholder="I am shopping for"></input>
+
+//       <img className="Cart" src={require("./img/mynaui_cart.png")} />
+
+//       <img className="Contact" src={require("./img/contact.png")} />
+//     </div>
+//   );
+// }
+
+// export default Header;
